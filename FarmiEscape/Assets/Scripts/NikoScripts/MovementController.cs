6b0cf4d17958dee6a45490e651aababfd2b17943
Mashadeve <74298578+Mashadeve@users.noreply.github.com>
2022-02-04 15:28:53 +0200

    asd

--- FarmiEscape/Assets/Scripts/NikoScripts/MovementController.cs
+++ FarmiEscape/Assets/Scripts/NikoScripts/MovementController.cs
@@ -11,17 +11,30 @@ public class MovementController : MonoBehaviour
 
     private void Update()
     {
+        WASDMovement();
+        MouseTurn();
+    }
+
+    private void WASDMovement()
+    {
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
 
 
         if (verticalInput > 0 || horizontalInput > 0)
         {
-            transform.position += new Vector3(horizontalInput, 0 ,verticalInput) * movementSpeed * Time.deltaTime;
+            transform.position += new Vector3(horizontalInput, 0, verticalInput) * movementSpeed * Time.deltaTime;
         }
         else if (verticalInput < 0 || horizontalInput < 0)
         {
-            transform.position -= new Vector3(-horizontalInput, 0,-verticalInput) * movementSpeed * Time.deltaTime;
+            transform.position -= new Vector3(-horizontalInput, 0, -verticalInput) * movementSpeed * Time.deltaTime;
         }
     }
+
+
+    private void MouseTurn()
+    {
+
+    }
+
 }
