d4d7a74155f00ac4a1221231e2cafaccbd09b4ab
Mashadeve <74298578+Mashadeve@users.noreply.github.com>
2022-02-04 15:27:21 +0200

    push

---
+++ FarmiEscape/Assets/Scripts/NikoScripts/MovementController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    public float movementSpeed;
+    public float horizontalInput, verticalInput;
+
+
+
+    private void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+
+        if (verticalInput > 0 || horizontalInput > 0)
+        {
+            transform.position += new Vector3(horizontalInput, 0 ,verticalInput) * movementSpeed * Time.deltaTime;
+        }
+        else if (verticalInput < 0 || horizontalInput < 0)
+        {
+            transform.position -= new Vector3(-horizontalInput, 0,-verticalInput) * movementSpeed * Time.deltaTime;
+        }
+    }
+}
