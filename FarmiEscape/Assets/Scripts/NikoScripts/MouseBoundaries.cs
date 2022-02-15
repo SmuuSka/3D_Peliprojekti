e796b4a8c50a1f87027402da7cf37721544f8d7b
Mashadeve <74298578+Mashadeve@users.noreply.github.com>
2022-02-15 12:01:49 +0200

    15.2 päivän lopetus :white_check_mark:

---
+++ FarmiEscape/Assets/Scripts/NikoScripts/MouseBoundaries.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class MouseBoundaries : MonoBehaviour
+{
+
+
+    private void Update()
+    {
+
+        if (NetworkManagerHUD.setMouseLock)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+}
