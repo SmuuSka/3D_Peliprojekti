c556aaee0f5f892e93c4d190e74a2efd13290834
Samu <65758544+SmuuSka@users.noreply.github.com>
2022-03-14 10:06:47 +0200

    testiukkeli

---
+++ FarmiEscape_clone_0/Assets/Scripts/SamunScriptit/TestiUkkeli/MoveScripts.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class MoveScripts : MonoBehaviour
+{
+    private Vector3 velocity = Vector3.zero;
+
+    private Rigidbody playerRb;
+
+    private void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    private void PerformMove()
+    {
+        if (velocity != Vector3.zero)
+        {
+            playerRb.MovePosition(playerRb.position + velocity * Time.fixedDeltaTime);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        PerformMove();
+    }
+}
